d7747e52b41276e3edb61d30e9456335eee66d3c
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2019-09-16 18:39:18 +0600

    создал Intent чтобы открыть другое активити

--- app/src/main/java/com/example/homework36/MainActivity.java
+++ app/src/main/java/com/example/homework36/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openActivity(View view) {
+        Intent intent=new Intent(MainActivity.this,TaskDetailsActivity.class);
+        startActivityForResult(intent,100);
     }
 }
