7feb36aa2c414a9d50f01e262a59d3e2c4005152
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2019-09-16 18:41:46 +0600

    создал вёрстку для второго активити activity_task_details.xml

--- app/src/main/java/com/example/homework36/TaskDetailsActivity.java
+++ app/src/main/java/com/example/homework36/TaskDetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.homework36;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class TaskDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class TaskDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+    }
+
+    public void onClickSave(View view) {
     }
 }
