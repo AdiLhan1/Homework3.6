2ac8870ac7a5d92d1446bc97bfb8dc0a95cd6261
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2019-09-16 18:37:18 +0600

    Инициализоровал Adapter, LayoutManager в MainActivity

--- app/src/main/java/com/example/homework36/MainActivity.java
+++ app/src/main/java/com/example/homework36/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.homework36;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button add;
+    TaskAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        add=findViewById(R.id.add);
+        RecyclerView recyclerView= findViewById(R.id.task_list);
+        adapter = new TaskAdapter();
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager manager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+
     }
 
     public void openActivity(View view) {
